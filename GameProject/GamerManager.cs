94c544c1d33f77cd5dfd9d005e1c457062569c3a
Mətləb Əliyev <metleb.aliyev.1997@bk.ru>
2022-10-01 16:42:49 +0400

    Last

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class GamerManager : IGamerService
+    {
+        IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer)==true)
+            {
+                Console.WriteLine("Kayit oldu");
+            }
+            else
+            {
+                Console.WriteLine("Dogrulama basarisiz...");
+            }
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayit Silindi");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayit Guncellendi");
+        }
+    }
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer { ID = 1, BirthYear = 1985, 
+                 FirstName = "ENGIN", LastName = "DEMIROG",IdentityNumber=12345 });
+
+        }
+    }
+}
